64cd022e6cf8131e42c22ac16914a3ce6ce60e4b
Connor Joel <cjoel4000@gmail.com>
2023-01-22 17:23:01 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Connor Joel on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
