83ea76f25737426cf0efda620db4fe0907a64da4
Connor Joel <cjoel4000@gmail.com>
2023-01-23 17:38:03 -0500

    Finished through 1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -3,19 +3,34 @@
 //  YouAreAwesome
 //
 //  Created by Connor Joel on 1/22/23.
-//
+// First Assignment
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var messageString = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .padding()
+            HStack {
+                Button("Awesome") {
+                    // This is the action
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    // This is the action
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
         }
-        .padding()
     }
 }
 
